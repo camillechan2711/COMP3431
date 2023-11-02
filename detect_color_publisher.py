ed32e795a9d7cd1df89f8c3aab3375e19f44e2fc
roll-alt233 <63288008+roll-alt233@users.noreply.github.com>
2023-11-02 20:24:19 +1100

    Update detect_color_publisher.py

--- detect_color_publisher.py
+++ detect_color_publisher.py
@@ -11,71 +11,48 @@ class ColorPublisher(Node):
     Create an ImagePublisher class, which is a subclass of the Node class.
     """
     def __init__(self):
-        """
-        Class constructor to set up the node
-        """
         super().__init__('color_publisher')
         
-        # Create the publisher for detected colors
         self.publisher_ = self.create_publisher(String, 'detected_colors_topic', 10)
+        self.position_publisher_ = self.create_publisher(String, 'detected_colors_positions', 10)
         
-        # Subscribe to the TurtleBot's camera topic
         self.subscription = self.create_subscription(
             Image,
             '/camera/rgb/image_raw',
             self.image_callback,
             10)
-        self.subscription  # prevent unused variable warning
+        self.subscription
         
-        # Used to convert between ROS and OpenCV images
         self.br = CvBridge()
 
+    def find_color_positions(self, mask, color_name, image):
+        contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        if contours:
+            max_contour = max(contours, key=cv2.contourArea)
+            x, y, w, h = cv2.boundingRect(max_contour)
+            cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            cv2.putText(image, color_name, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+            return f"{color_name}: x={x}, y={y}, w={w}, h={h}"
+        return None
+
     def image_callback(self, msg):
-        """
-        Callback function.
-        This function gets called upon receiving an image message from the TurtleBot's camera.
-        """
-        # Convert ROS Image message to OpenCV image
         cv_image = self.br.imgmsg_to_cv2(msg, desired_encoding='bgr8')
-        
-        # Convert to HSV
         hsv_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2HSV)
 
-        # Define color ranges
-        lower_pink = np.array([0, 100, 124])
-        upper_pink = np.array([163, 234, 226])
-        lower_yellow = np.array([0, 81, 62])
-        upper_yellow = np.array([103, 237, 128])
-        lower_green = np.array([0, 93, 62])
-        upper_green = np.array([81, 235, 255])
+        # ... [rest of the color detection code]
 
-        # Create masks for each color
-        mask_pink = cv2.inRange(hsv_image, lower_pink, upper_pink)
-        mask_yellow = cv2.inRange(hsv_image, lower_yellow, upper_yellow)
-        mask_green = cv2.inRange(hsv_image, lower_green, upper_green)
+        pink_position = self.find_color_positions(mask_pink, 'Pink', cv_image)
+        green_position = self.find_color_positions(mask_green, 'Green', cv_image)
 
-        # Check the number of non-zero pixels for each mask
-        non_zero_pink = cv2.countNonZero(mask_pink)
-        non_zero_yellow = cv2.countNonZero(mask_yellow)
-        non_zero_green = cv2.countNonZero(mask_green)
+        # Publish the positions if detected
+        if pink_position:
+            self.position_publisher_.publish(pink_position)
+        if green_position:
+            self.position_publisher_.publish(green_position)
 
-        threshold = 150
-        detected_colors = []
+        cv2.imshow('Result', cv_image)
+        cv2.waitKey(1)
 
-        # Determine the detected colors
-        if non_zero_pink > threshold:
-            detected_colors.append('Pink')
-        if non_zero_yellow > threshold:
-            detected_colors.append('Yellow')
-        if non_zero_green > threshold:
-            detected_colors.append('Green')
-
-        # Publish the detected colors
-        msg = String()
-        msg.data = ', '.join(detected_colors) if detected_colors else 'No colors detected'
-        self.publisher_.publish(msg)
-        
-        # Display the message on the console
         self.get_logger().info('Publishing: "%s"' % msg.data)
 
 def main(args=None):
