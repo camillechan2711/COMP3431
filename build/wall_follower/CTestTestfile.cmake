25c6b72f73550b086dabfb2416eab21da4d5ddf6
reiji99 <reiji.semidang@gmail.com>
2023-11-07 17:53:58 -0800

    yeah

---
+++ build/wall_follower/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/rsa/tmp/COMP3431/src/wall_follower
+# Build directory: /home/rsa/tmp/COMP3431/build/wall_follower
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
