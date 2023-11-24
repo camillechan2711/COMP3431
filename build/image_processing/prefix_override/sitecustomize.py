dab10bd7059af99503fa84495f958fe9ea2bf34e
camille <camillechan2711@gmail.com>
2023-11-24 14:33:19 +0000

    preparing repo for submission

--- build/image_processing/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/comp3431/colcon4_ws/COMP3431/install/image_processing'
--- build/wall_follower/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/comp3431/colcon4_ws/COMP3431/src/wall_follower
-# Build directory: /home/comp3431/colcon4_ws/COMP3431/build/wall_follower
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
